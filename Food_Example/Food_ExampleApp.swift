a49920bdcc70e3ff7d02e7d4f75c9a8231d7fd26
Arthur <arthur@blackbricks.io>
2023-01-11 15:24:32 +0300

    Initial Commit

---
+++ Food_Example/Food_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_ExampleApp.swift
+//  Food_Example
+//
+//  Created by Артур Кулик on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Food_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
