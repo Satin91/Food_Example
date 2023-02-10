78c0c747e1035abdd17b2d47fdc712ad38aac86a
Satin91 <cool19912015@gmail.com>
2023-02-10 17:58:31 +0300

    *Change Recipe Model for Realm conformity

--- Food_Example/Models/Recipe.swift
+++ Food_Example/Models/Recipe.swift
@@ -18,21 +18,8 @@ struct Recipe: Identifiable {
     var id: Int
     var title: String
     var image: String
-    var vegetarian: Bool?
-    var vegan: Bool?
-    var glutenFree: Bool?
-    var dairyFree: Bool?
-    var veryPopular: Bool?
-    var sustainable: Bool?
-    var veryHealthy: Bool?
-    var lowFodmap: Bool?
-    var weightWatcherSmartPoints: Int?
-    var gaps: String?
     var preparationMinutes: Int?
     var cookingMinutes: Int?
-    var aggregateLikes: Int?
-    var healthScore: Int?
-    var pricePerServing: Double?
     var extendedIngredients: [ExtendedIngredient]?
     var nutrients: Nutritient?
     var ingridients: [Ingredient]?
@@ -58,11 +45,6 @@ struct ExtendedIngredient: Decodable {
     let amount: Double?
     let unit: String?
     let meta: [String?]
-}
-
-enum Consistency: Decodable {
-    case liquid
-    case solid
 }
 
 struct Nutritient: Decodable {
@@ -137,21 +119,8 @@ extension Recipe: Decodable {
         case id
         case title
         case image
-        case vegetarian
-        case vegan
-        case glutenFree
-        case dairyFree
-        case veryPopular
-        case sustainable
-        case veryHealthy
-        case lowFodmap
-        case weightWatcherSmartPoints
-        case gaps
         case preparationMinutes
         case cookingMinutes
-        case aggregateLikes
-        case healthScore
-        case pricePerServing
         case nutrients
         case extendedIngredients
         case ingridients
@@ -165,21 +134,8 @@ extension Recipe: Decodable {
         self.id = try container.decode(Int.self, forKey: .id)
         self.title = try container.decode(String.self, forKey: .title)
         self.image = try container.decode(String.self, forKey: .image)
-        self.vegetarian = try container.decodeIfPresent(Bool.self, forKey: .vegetarian)
-        self.vegan = try container.decodeIfPresent(Bool.self, forKey: .vegan)
-        self.glutenFree = try container.decodeIfPresent(Bool.self, forKey: .glutenFree)
-        self.dairyFree = try container.decodeIfPresent(Bool.self, forKey: .dairyFree)
-        self.veryPopular = try container.decodeIfPresent(Bool.self, forKey: .veryPopular)
-        self.sustainable = try container.decodeIfPresent(Bool.self, forKey: .sustainable)
-        self.veryHealthy = try container.decodeIfPresent(Bool.self, forKey: .veryHealthy)
-        self.lowFodmap = try container.decodeIfPresent(Bool.self, forKey: .lowFodmap)
-        self.weightWatcherSmartPoints = try container.decodeIfPresent(Int.self, forKey: .weightWatcherSmartPoints)
-        self.gaps = try container.decodeIfPresent(String.self, forKey: .gaps)
         self.preparationMinutes = try container.decodeIfPresent(Int.self, forKey: .preparationMinutes)
         self.cookingMinutes = try container.decodeIfPresent(Int.self, forKey: .cookingMinutes)
-        self.aggregateLikes = try container.decodeIfPresent(Int.self, forKey: .aggregateLikes)
-        self.healthScore = try container.decodeIfPresent(Int.self, forKey: .healthScore)
-        self.pricePerServing = try container.decodeIfPresent(Double.self, forKey: .pricePerServing)
         self.extendedIngredients = try container.decodeIfPresent([ExtendedIngredient].self, forKey: .extendedIngredients)
         self.nutrients = try container.decodeIfPresent(Nutritient.self, forKey: .nutrients)
         self.ingridients = try container.decodeIfPresent([Ingredient].self, forKey: .ingridients)
